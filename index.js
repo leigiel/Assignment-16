fc1757408ca1f5e5e58865d259215599ec351df9
leigiel <nasifabdullahp@gmail.com>
2024-10-17 20:06:44 +0600

    final commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// console.log("this is from main")
+
+// function clickMe(){
+//     console.log('paise')
+// }
+
+// function clickMe(){
+//     document.body.style.backgroundColor = "red";
+// }
+
+//addEventListener
+
+// const btn = document.getElementById('green').addEventListener('click',document.body.style.backgroundColor = 'green')
+// console.log(btn)
+
+// document.getElementById('green').addEventListener('click',document.body.style.backgroundColor = 'green')
+// console.log(green)
+
+// document.getElementById('green').addEventListener('click',function clr(){
+//     document.body.style.backgroundColor = "green"
+// }
+// )
+
+// document.getElementById('green').addEventListener('click',clr())
+// function clr(){
+// document.body.style.backgroundColor = "green"
+// }
+
+// const title = document.getElementById('green')
+// title.addEventListener('click',function clr(){
+//     document.body.style.backgroundColor = "green"
+// }
+// )
+
+// const title = document.getElementById('green')
+// title.addEventListener('click',function (){
+//     const para = document.getElementById('para')
+//     console.log(para.innerText)
+// }
+// )
+
+// const title = document.getElementById('btn')
+// title.addEventListener('click',function (){
+//     const para = document.getElementById('para')
+//     para.innerText = "Hi i am Nasif"
+// }
+// )
+
+// function nasif(values){
+//     console.log(values)
+// }
+// nayeem(40)
